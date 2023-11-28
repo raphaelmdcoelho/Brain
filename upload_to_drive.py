3ce61ade0192512569939b7dfe1d7d5c34650829
Raphael Coelho <rmdcoelho@gmail.com>
2023-11-28 10:22:31 -0300

    Add fixes

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,7 +1,7 @@
 import os
+from googleapiclient.http import MediaFileUpload
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
 
 def get_drive_service():
     creds = service_account.Credentials.from_service_account_file(
@@ -10,16 +10,28 @@ def get_drive_service():
     )
     return build('drive', 'v3', credentials=creds)
 
-def upload_file(filename, filepath, mimetype, folder_id):
-    service = get_drive_service()
-    file_metadata = {
-        'name': filename,
-        'parents': [folder_id]  # Add the folder ID here
-    }
-    media = MediaFileUpload(filepath, mimetype=mimetype)
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print(f"File ID: {file.get('id')}")
+def find_file(service, name, folder_id):
+    response = service.files().list(q=f"name='{name}' and '{folder_id}' in parents",
+                                    spaces='drive',
+                                    fields='files(id, name)').execute()
+    for file in response.get('files', []):
+        # Assuming first match is the file to update
+        return file.get('id')
+    return None
 
-# Call the function with the folder ID
-folder_id = '1dQiC-8K0yjKZMuQ97Q87JfUsgg1lBGpI'  # Replace with your actual folder ID
-upload_file('markdown_files.zip', 'markdown_files.zip', 'application/zip', folder_id)
+def upload_or_update_file(filename, filepath, mimetype, folder_id):
+    service = get_drive_service()
+    file_id = find_file(service, filename, folder_id)
+
+    file_metadata = {'name': filename, 'parents': [folder_id]}
+    media = MediaFileUpload(filepath, mimetype=mimetype)
+
+    if file_id:
+        updated_file = service.files().update(fileId=file_id, body=file_metadata, media_body=media).execute()
+        print(f"Updated File ID: {updated_file.get('id')}")
+    else:
+        new_file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        print(f"New File ID: {new_file.get('id')}")
+
+folder_id = 'your-folder-id-here'  # Replace with your actual folder ID
+upload_or_update_file('markdown_files.zip', 'markdown_files.zip', 'application/zip', folder_id)
